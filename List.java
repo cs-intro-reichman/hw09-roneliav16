d92fe87cfe541731d23e1416e4e7c38bac98f811
Ron Eliav <ron.eliav@post.runi.ac.il>
2024-02-21 18:54:44 +0200

    Second Check

--- List.java
+++ List.java
@@ -24,7 +24,7 @@ public class List {
 
     /** Returns the first element in the list */
     public CharData getFirst() {
-        return first.cp;
+        return first;
     }
 
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
